16ab810412a1ffeafffd502ccaa64e78ab1b16d2
donoghuc <cas.donoghue@gmail.com>
2020-06-09 15:37:07 -0700

    generic

---
+++ site-modules/remote_tasks/lib/puppet/provider/remote_tasks/remote_tasks.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'puppet/resource_api'
+require 'puppet/resource_api/simple_provider'
+
+class Puppet::Provider::RemoteTasks::RemoteTasks < Puppet::ResourceApi::SimpleProvider
+
+end
